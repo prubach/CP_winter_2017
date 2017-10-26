55f99fd3447360b3f87e7924a6544980152879cf
prubac <prubac@sgh.waw.pl>
2017-10-26 13:55:07 +0200

    Email Check Stub

---
+++ src/pl/waw/sgh/EmailCheck.java
@@ -0,0 +1,12 @@
+package pl.waw.sgh;
+
+public class EmailCheck {
+    public static void main(String[] args) {
+        final String EMAIL_OK = "user@sgh.pl";
+        final String EMAIL_NOT_OK1 = "@sgh.pl";
+
+        String email = EMAIL_NOT_OK1;
+
+
+    }
+}
