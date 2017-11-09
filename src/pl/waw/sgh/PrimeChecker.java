a0551b18e5fa3b7f62ab018382ea9120263a2be9
prubac <prubac@sgh.waw.pl>
2017-11-09 15:13:31 +0100

    Email Check Stub

---
+++ src/pl/waw/sgh/PrimeChecker.java
@@ -0,0 +1,18 @@
+package pl.waw.sgh;
+
+public class PrimeChecker {
+
+    public static boolean isPrime(int number) {
+        // TODO
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+
+        int n = 65;
+        boolean nIsPrime = isPrime(n);
+        System.out.println("n: "  + n + " " + nIsPrime);
+    }
+
+}
