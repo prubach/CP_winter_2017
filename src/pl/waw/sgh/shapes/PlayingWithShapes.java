344283d68aa515596403b5ba575546c4735c7f74
prubac <prubac@sgh.waw.pl>
2017-12-07 12:34:58 +0100

    Fixing calling constructos in PlayingWithShapes

--- src/pl/waw/sgh/shapes/PlayingWithShapes.java
+++ src/pl/waw/sgh/shapes/PlayingWithShapes.java
@@ -3,9 +3,10 @@ package pl.waw.sgh.shapes;
 public class PlayingWithShapes {
 
     public static void main(String[] args) {
-        Rectangle r1 = new Rectangle();
-        //r1.setParams(4,6);
         Params params = new Params(4,6);
+        Rectangle r1 = new Rectangle(params);
+        //r1.setParams(4,6);
+
         Params params1 = new Params();
 
         ParamsSimple parSim = new ParamsSimple();
@@ -18,7 +19,7 @@ public class PlayingWithShapes {
         //params.setParA(54);
 
         r1.setParams(params);
-        Rectangle r2 = new Rectangle();
+        Rectangle r2 = new Rectangle(new Params(3,4));
         //r2.setParams(new Params(4, 6));
         r2.setParams(params);
 
@@ -27,7 +28,7 @@ public class PlayingWithShapes {
 /*        if (r1.isEqual(r2))
             System.out.println("r1 isEqual to r2");
 */
-        Circle c1 = new Circle();
+        Circle c1 = new Circle(new Params(3));
         //System.out.println(r1.calcSurface());
         System.out.println(r1);
         System.out.println(c1);
@@ -38,9 +39,9 @@ public class PlayingWithShapes {
         Shape[] shapes = new Shape[4];
         shapes[0] = r1;
         shapes[1] = c1;
-        shapes[2] = new Circle();
+        shapes[2] = new Circle(new Params(6));
         shapes[2].setParams(2);
-        shapes[3] = new NewRectangle();
+        shapes[3] = new NewRectangle(new Params(3,5));
 
         for (Shape s : shapes) {
             System.out.println("Shape: " + s);
