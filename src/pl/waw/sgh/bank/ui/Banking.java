84a48145f985a1a3fd466d148844ec74133aec11
prubac <prubac@sgh.waw.pl>
2018-01-04 14:59:30 +0100

    UI - using Intellij

---
+++ src/pl/waw/sgh/bank/ui/Banking.java
@@ -0,0 +1,13 @@
+package pl.waw.sgh.bank.ui;
+
+import javax.swing.*;
+
+public class Banking {
+
+    public static void main(String[] args) {
+        JFrame myBank = new JFrame("My Bank");
+        BankingApp bankingApp = new BankingApp();
+        myBank.add(bankingApp.getMainPanel());
+        myBank.setVisible(true);
+    }
+}
---
+++ src/pl/waw/sgh/bank/ui/BankingApp.java
@@ -0,0 +1,93 @@
+package pl.waw.sgh.bank.ui;
+
+import javax.swing.*;
+
+public class BankingApp {
+    private JTabbedPane tabbedPane1;
+    private JPanel mainPanel;
+
+    public JTabbedPane getTabbedPane1() {
+        return tabbedPane1;
+    }
+
+
+    public void setTabbedPane1(JTabbedPane tabbedPane1) {
+        this.tabbedPane1 = tabbedPane1;
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public void setMainPanel(JPanel mainPanel) {
+        this.mainPanel = mainPanel;
+    }
+
+    public JTextField getFromAccField() {
+        return fromAccField;
+    }
+
+    public void setFromAccField(JTextField fromAccField) {
+        this.fromAccField = fromAccField;
+    }
+
+    public JTextField getToAccField() {
+        return toAccField;
+    }
+
+    public void setToAccField(JTextField toAccField) {
+        this.toAccField = toAccField;
+    }
+
+    public JTextField getAmountField() {
+        return amountField;
+    }
+
+    public void setAmountField(JTextField amountField) {
+        this.amountField = amountField;
+    }
+
+    public JButton getGoButton() {
+        return goButton;
+    }
+
+    public void setGoButton(JButton goButton) {
+        this.goButton = goButton;
+    }
+
+    public JLabel getFromAccLabel() {
+        return fromAccLabel;
+    }
+
+    public void setFromAccLabel(JLabel fromAccLabel) {
+        this.fromAccLabel = fromAccLabel;
+    }
+
+    public JLabel getToAccLabel() {
+        return toAccLabel;
+    }
+
+    public void setToAccLabel(JLabel toAccLabel) {
+        this.toAccLabel = toAccLabel;
+    }
+
+    public JLabel getAmountLabel() {
+        return amountLabel;
+    }
+
+    public void setAmountLabel(JLabel amountLabel) {
+        this.amountLabel = amountLabel;
+    }
+
+    private JTextField fromAccField;
+    private JTextField toAccField;
+    private JTextField amountField;
+    private JButton goButton;
+    private JLabel fromAccLabel;
+    private JLabel toAccLabel;
+    private JLabel amountLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
